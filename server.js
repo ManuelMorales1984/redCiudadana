51e9b6826693095bdbe1a99530afc32d8f00d2b0
@Nelo240 <manuel.morales@meltsan.com>
2017-11-25 17:48:56 -0600

    cambio carpeta

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-console.log("Manuelito ¨.:");
 var http = require('http');
 var server = http.createServer();
+var port = process.env.PORT || 8000
 function control(petic, resp) {
 resp.writeHead(200, {'content-type': 'text/plain'});
 resp.write('Hola, Mundo!');
 resp.end();
 }
 server.on('request', control);
-server.listen(8080);
+server.listen(port,function(){
+
+	console.log('Puerto utilziado : ' + port);
+});
