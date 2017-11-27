6e8cc66d6a7db7d946a50ae8e51887b3cac35582
@Nelo240 <manuel.morales@meltsan.com>
2017-11-26 18:28:43 -0600

    cambios imp,emnetacio a express

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-var http = require('http');
-var server = http.createServer();
-var port = process.env.PORT || 8000
-function control(petic, resp) {
-resp.writeHead(200, {'content-type': 'text/plain'});
-resp.write('Hola, Mundo!');
-resp.end();
-}
-server.on('request', control);
-server.listen(port,function(){
 
-	console.log('Puerto utilziado : ' + port);
+var express = require("express"); 
+var app = express(); 
+var port = process.env.PORT || 8000;
+app.get('/', function(req, res) { 
+	res.send('Geeky Theory probando express.js'); 
 });
+app.get('/prueba', function(req, res) {
+ res.send('Geeky Theory probando express.js en /prueba'); 
+});
+ app.listen(port);
+ console.log("levantando server con express");
+
+
