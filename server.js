dd04df0c90b86a7c32bdce179868580bd2834b2b
@Nelo240 <manuel.morales@meltsan.com>
2017-11-26 18:47:36 -0600

    cambios imp,emnetacio a express

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 
 var express = require("express"); 
+var body_parser = require('body-parser');
 var app = express(); 
+app.use(body_parser.urlencoded({extended:true}));
 var port = process.env.PORT || 8000;
+
 app.get('/', function(req, res) { 
 	res.send('Geeky Theory probando express.js'); 
 });
 app.post('/prueba', function(req, res) {
+	console.log(req.body.nombre);
  console.log("Manueliito Probando parametros de entrada");
- console.log(req);
+ var nombre = req.body.nombre || '';
+ 
  res.send('Geeky Theory probando express.js en /prueba'); 
 });
  app.listen(port);
